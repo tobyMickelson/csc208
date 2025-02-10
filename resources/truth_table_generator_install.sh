f943af070b32e43a7638c0cdf030ae09119bb9c1
Toby Mickelson <tmickelson@tinyfern.com>
2025-02-10 08:54:23 -0500

    change script

--- resources/truth_table_generator_install.sh
+++ resources/truth_table_generator_install.sh
@@ -8,7 +8,8 @@ case "$choice" in
 esac
 
 # install truth-table-generator using the system pip3.9
-/Library/Developer/CommandLineTools/usr/bin/python3.9 -m pip install --upgrade truth-table-generator
+/Library/Developer/CommandLineTools/usr/bin/python3.9 -m pip install --quiet --quiet --upgrade truth-table-generator
 
 # expose ttg_cli.py in the $PATH
 echo "export PATH='$PATH:$HOME/Library/Python/3.9/bin'" >> ~/.zshrc # if you use a different shell i'm sure you'll figure it out
+source ~/.zshrc
